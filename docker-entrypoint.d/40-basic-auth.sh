b7f4d1da53f6062bb6e57e23c686b58de62db48e
Krause Robotics, LLC <140582805+KrauseRobotics@users.noreply.github.com>
2025-12-14 17:25:27 -0500

    Revert "Merge pull request #1 from Chad-Krause/codex/create-dockerfile-for-nginx-website"
    This reverts commit 5ccaca0d8230264a9edf4e8ed93c54d6bf82e91c, reversing
    changes made to 33ddfdbcd4c8db7f90a87a20b2938bfc77d98421.

--- docker-entrypoint.d/40-basic-auth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -z "$BASIC_AUTH_USER" ] || [ -z "$BASIC_AUTH_PASSWORD" ]; then
-    echo "Error: BASIC_AUTH_USER and BASIC_AUTH_PASSWORD must be set." >&2
-    exit 1
-fi
-
-htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASSWORD"
